ae81bfacc13e6905e55b9d91b04252a2bec51133
aimandurrani18 <aimand1704@gmail.com>
2025-09-09 18:30:59 +0530

    Second commit

--- test_app.py
+++ test_app.py
@@ -7,6 +7,7 @@ class TestApp(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
         self.assertEqual(add(1, 1), 2)
+        self.assertEqual(add(4, 1), 5)
 
 if _name_ == "_main_":
     unittest.main()
